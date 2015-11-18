b5743b3f4923443ec6a5468a9215399881b760ed
Kristian <kristian@viscaweb.com>
2015-11-18 11:07:06 +0100

    LIFE-922: MatchListingWidget - Added Constant values for the MatchAuxProfile to know the importance of the match

---
+++ Entity/Value/MatchAuxProfileValue.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Entity\Value;
+
+/**
+ * Class MatchAuxProfileValue
+ *
+ * This class is to enumerate all the types of ProfileValues
+ * Is not a DB enum field, its only to not use strings inside the code.
+ */
+class MatchAuxProfileValue
+{
+    const IMPORTANT = 'important';
+    const TOP = 'top';
+    const SECOND = '2nd';
+}
