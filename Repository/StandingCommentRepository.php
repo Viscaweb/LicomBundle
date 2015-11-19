0c157672335fe31036e4a3020be7826fd1d0782f
Raúl Ferràs <raul@viscaweb.com>
2015-11-19 16:57:22 +0100

    StandingCommentRepository: Implement method `findByStandingRow`.

--- Repository/StandingCommentRepository.php
+++ Repository/StandingCommentRepository.php
@@ -3,10 +3,33 @@
 
 namespace Visca\Bundle\LicomBundle\Repository;
 
+use Doctrine\ORM\Query\Expr\Join;
+use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
+use Visca\Bundle\LicomBundle\Entity\Code\StandingCommentGraphLabelCode;
+use Visca\Bundle\LicomBundle\Entity\StandingComment;
+use Visca\Bundle\LicomBundle\Entity\StandingRow;
+
 /**
  * Class StandingCommentRepository
  */
-class StandingCommentRepository
+class StandingCommentRepository extends AbstractEntityRepository
 {
-
+    /**
+     * @param StandingRow $standingRow StandingRow
+     *
+     * @return StandingComment
+     */
+    public function findByStandingRow(StandingRow $standingRow)
+    {
+        return $this->createQueryBuilder('sc')
+            ->join('ViscaLicomBundle:StandingCommentGraph', 'scg', Join::WITH, 'sc.id = scg.standingComment')
+            ->where('scg.standingRow = :standingRow')
+            ->andWhere('scg.label = :label')
+            ->setParameters([
+                'standingRow' => $standingRow,
+                'label' => StandingCommentGraphLabelCode::DEFAULT_CODE
+            ])
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
