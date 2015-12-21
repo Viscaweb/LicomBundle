4930241782de7046b09eafac4d49a49f2a5cd6f0
Ricard Osorio <celtric@gmail.com>
2015-12-21 14:50:22 +0100

    add new repository for Standing Type - define it in services for auto creation

---
+++ Repository/StandingTypeRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Repository;
+
+use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
+
+/**
+ * Class StandingTypeRepository.
+ */
+class StandingTypeRepository extends AbstractEntityRepository
+{
+
+}
