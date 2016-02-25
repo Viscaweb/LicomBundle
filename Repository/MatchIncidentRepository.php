50c6b8ee81cfa77bb083c89dfd5b4f6306342f80
Raúl Ferràs <raul@viscaweb.com>
2016-02-25 11:54:01 +0100

    New models added to the MatchIncident system. Adaptions made.

--- Repository/MatchIncidentRepository.php
+++ Repository/MatchIncidentRepository.php
@@ -2,7 +2,10 @@
 
 namespace Visca\Bundle\LicomBundle\Repository;
 
+use Doctrine\ORM\Query\Expr\Join;
 use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
+use Visca\Bundle\LicomBundle\Entity\Match;
+use Visca\Bundle\LicomBundle\Entity\MatchIncident;
 use Visca\Bundle\LicomBundle\Entity\MatchIncidentType;
 use Visca\Bundle\LicomBundle\Entity\MatchParticipant;
 
@@ -11,6 +14,44 @@ use Visca\Bundle\LicomBundle\Entity\MatchParticipant;
  */
 class MatchIncidentRepository extends AbstractEntityRepository
 {
+    /**
+     * Gets the incidents of a match, eager loading usual related entities.
+     *
+     * @param Match $match              Match entity
+     * @param int[] $matchIncidentTypes MatchIncidentTypes if we want to filter
+     *                                  the incidents to retrieve by its type.
+     *
+     * @return \Visca\Bundle\LicomBundle\Entity\MatchIncident[]
+     */
+    public function findByMatch(Match $match, $matchIncidentTypes = array())
+    {
+        $queryBuilder = $this->createQueryBuilder('mi');
+
+        $queryBuilder
+            ->select('mi', 'aux', 'mp', 'p')
+            ->leftJoin('mi.matchIncidentAux', 'aux')
+            ->join('mi.matchParticipant', 'mp')
+            ->leftJoin('mi.participant', 'p')
+            ->join('mp.match', 'm', Join::WITH, 'm.id = :matchId')
+            ->setParameters(
+                [
+                    'matchId' => $match->getId()
+                ]
+            );
+
+        if (count($matchIncidentTypes) > 0) {
+            $queryBuilder
+                ->setParameter('matchIncidentType', $matchIncidentTypes)
+                ->where('mi.matchIncidentType IN (:matchIncidentType)');
+        }
+
+        $queryBuilder
+            ->orderBy('mi.time', 'ASC')
+            ->addOrderBy('mi.position', 'ASC');
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
     /**
      * @param MatchParticipant    $matchParticipant MatchParticipant
      * @param MatchIncidentType[] $typeList         MatchIncidentType[]
