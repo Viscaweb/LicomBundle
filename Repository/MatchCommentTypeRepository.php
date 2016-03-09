829a7feada46a2378a94c60f3da90dde09d3bb1b
Ricard Osorio <celtric@gmail.com>
2016-03-09 10:53:18 +0100

    add MatchCommentTypeRepository

---
+++ Repository/MatchCommentTypeRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Repository;
+
+use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
+use Visca\Bundle\LicomBundle\Entity\MatchComment;
+
+/**
+ * Class MatchCommentTypeRepository.
+ */
+class MatchCommentTypeRepository extends AbstractEntityRepository
+{
+}
