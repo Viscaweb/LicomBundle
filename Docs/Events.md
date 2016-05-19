0a6001445b7c4d0b7b7866aa527fa7a667339c72
Jonas HAOUZI <jonas@viscaweb.com>
2016-05-19 14:13:21 +0200

    Supporting the event MatchStats

--- Docs/Events.md
+++ Docs/Events.md
@@ -8,3 +8,4 @@
 |MatchResult  |✓|✓|✓| |
 |MatchIncident|✓|✓|✓|✓|
 |MatchComment |✓| | | |
+|MatchStats   |✓|✓|✓| |
---
+++ Events/Match/MatchStats.php
@@ -0,0 +1,33 @@
+<?php
+namespace Visca\Bundle\LicomBundle\Events\Match;
+
+use Visca\Bundle\LicomBundle\Entity\Competition;
+use Visca\Bundle\LicomBundle\Entity\Match as LicomMatch;
+use Visca\Bundle\LicomBundle\Entity\Team;
+use Visca\Bundle\LicomBundle\Events\AbstractEvent;
+
+class MatchStats extends AbstractEvent
+{
+    public static function listenByMatch(LicomMatch $match)
+    {
+        return new static('match.'.$match->getId());
+    }
+
+    public static function listenByCompetition(Competition $competition)
+    {
+        return new static('competition.'.$competition->getId());
+    }
+
+    public static function listenByTeam(Team $team)
+    {
+        return new static('team.'.$team->getId());
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventObject()
+    {
+        return 'match_stats';
+    }
+}
