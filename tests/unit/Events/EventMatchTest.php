27a3bd8d4e396eebc10c67f6a899131f99e15c58
Jonas HAOUZI <jonas@viscaweb.com>
2016-05-25 17:56:34 +0200

    Completing the tests and updating the database

--- tests/unit/Events/EventMatchTest.php
+++ tests/unit/Events/EventMatchTest.php
@@ -49,55 +49,55 @@ class EventMatchTest extends PHPUnit_Framework_TestCase
 
         return [
             // Match
-            [$matchEvent,         'listenByMatch',       $this->createMatch(1),       'match@match.1'],
-            [$matchEvent,         'listenByCompetition', $this->createCompetition(1), 'match@competition.1'],
-            [$matchEvent,         $listenByCompSS,       $competitionSS,              'match@competition_season_stage.1'],
-            [$matchEvent,         'listenByTeam',        $this->createTeam(1),        'match@team.1'],
+            [$matchEvent,         'listenByMatch',          $this->createMatch(1),                  'match@match.1'],
+            [$matchEvent,         'listenByCompetition',    $this->createCompetition(1),            'match@competition.1'],
+            [$matchEvent,         $listenByCompSS,          $competitionSS,                         'match@competition_season_stage.1'],
+            [$matchEvent,         'listenByTeam',           $this->createTeam(1),                   'match@team.1'],
 
             // MatchResult
-            [$matchResultEvent,   'listenByMatch',       $this->createMatch(1),       'match_result@match.1'],
-            [$matchResultEvent,   'listenByCompetition', $this->createCompetition(1), 'match_result@competition.1'],
-            [$matchResultEvent,   'listenByTeam',        $this->createTeam(1),        'match_result@team.1'],
+            [$matchResultEvent,   'listenByMatch',          $this->createMatch(1),                  'match_result@match.1'],
+            [$matchResultEvent,   'listenByCompetition',    $this->createCompetition(1),            'match_result@competition.1'],
+            [$matchResultEvent,   'listenByTeam',           $this->createTeam(1),                   'match_result@team.1'],
 
             // MatchIncident
-            [$matchIncidentEvent, 'listenByMatch',       $this->createMatch(1),       'match_incident@match.1'],
-            [$matchIncidentEvent, 'listenByCompetition', $this->createCompetition(1), 'match_incident@competition.1'],
-            [$matchIncidentEvent, 'listenByTeam',        $this->createTeam(1),        'match_incident@team.1'],
-            [$matchIncidentEvent, 'listenByAthlete',     $this->createAthlete(1),     'match_incident@athlete.1'],
+            [$matchIncidentEvent, 'listenByMatch',          $this->createMatch(1),                  'match_incident@match.1'],
+            [$matchIncidentEvent, 'listenByCompetition',    $this->createCompetition(1),            'match_incident@competition.1'],
+            [$matchIncidentEvent, 'listenByTeam',           $this->createTeam(1),                   'match_incident@team.1'],
+            [$matchIncidentEvent, 'listenByAthlete',        $this->createAthlete(1),                'match_incident@athlete.1'],
 
             // MatchCard
-            [$matchCardEvent,     'listenByMatch',       $this->createMatch(1),       'match_incident_card@match.1'],
-            [$matchCardEvent,     'listenByCompetition', $this->createCompetition(1), 'match_incident_card@competition.1'],
-            [$matchCardEvent,     'listenByTeam',        $this->createTeam(1),        'match_incident_card@team.1'],
-            [$matchCardEvent,     'listenByAthlete',     $this->createAthlete(1),     'match_incident_card@athlete.1'],
+            [$matchCardEvent,     'listenByMatch',          $this->createMatch(1),                  'match_incident_card@match.1'],
+            [$matchCardEvent,     'listenByCompetition',    $this->createCompetition(1),            'match_incident_card@competition.1'],
+            [$matchCardEvent,     'listenByTeam',           $this->createTeam(1),                   'match_incident_card@team.1'],
+            [$matchCardEvent,     'listenByAthlete',        $this->createAthlete(1),                'match_incident_card@athlete.1'],
 
             // MatchComment
-            [$matchCommentEvent,  'listenByMatch',       $this->createMatch(1),       'match_comment@match.1'],
+            [$matchCommentEvent,  'listenByMatch',          $this->createMatch(1),                  'match_comment@match.1'],
 
             // MatchHasBegun
-            [$matchHasBegunEvent, 'listenByMatch',       $this->createMatch(1),       'match_has_begun@match.1'],
-            [$matchHasBegunEvent, 'listenByCompetition', $this->createCompetition(1), 'match_has_begun@competition.1'],
-            [$matchHasBegunEvent, 'listenByCompetitionSeasonStage', $this->createCompetitionSeasonStage(1), 'match_has_begun@competition_season_stage.1'],
-            [$matchHasBegunEvent, 'listenByTeam',        $this->createTeam(1),        'match_has_begun@team.1'],
-            [$matchHasBegunEvent, 'listenBySport',        $this->createSport(1),        'match_has_begun@sport.1'],
+            [$matchHasBegunEvent, 'listenByMatch',          $this->createMatch(1),                  'match_has_begun@match.1'],
+            [$matchHasBegunEvent, 'listenByCompetition',    $this->createCompetition(1),            'match_has_begun@competition.1'],
+            [$matchHasBegunEvent, $listenByCompSS,          $this->createCompetitionSeasonStage(1), 'match_has_begun@competition_season_stage.1'],
+            [$matchHasBegunEvent, 'listenByTeam',           $this->createTeam(1),                   'match_has_begun@team.1'],
+            [$matchHasBegunEvent, 'listenBySport',          $this->createSport(1),                  'match_has_begun@sport.1'],
 
             // MatchHasFinished
-            [$matchHasFinishedEvent, 'listenByMatch',       $this->createMatch(1),       'match_has_finished@match.1'],
-            [$matchHasFinishedEvent, 'listenByCompetition', $this->createCompetition(1), 'match_has_finished@competition.1'],
-            [$matchHasFinishedEvent, 'listenByCompetitionSeasonStage', $this->createCompetitionSeasonStage(1), 'match_has_finished@competition_season_stage.1'],
-            [$matchHasFinishedEvent, 'listenByTeam',        $this->createTeam(1),        'match_has_finished@team.1'],
-            [$matchHasFinishedEvent, 'listenBySport',        $this->createSport(1),        'match_has_finished@sport.1'],
+            [$matchHasFinishedEvent, 'listenByMatch',       $this->createMatch(1),                  'match_has_finished@match.1'],
+            [$matchHasFinishedEvent, 'listenByCompetition', $this->createCompetition(1),            'match_has_finished@competition.1'],
+            [$matchHasFinishedEvent, $listenByCompSS,       $this->createCompetitionSeasonStage(1), 'match_has_finished@competition_season_stage.1'],
+            [$matchHasFinishedEvent, 'listenByTeam',        $this->createTeam(1),                   'match_has_finished@team.1'],
+            [$matchHasFinishedEvent, 'listenBySport',       $this->createSport(1),                  'match_has_finished@sport.1'],
 
             // MatchStats
-            [$matchStatsEvent,    'listenByMatch',       $this->createMatch(1),       'match_stats@match.1'],
-            [$matchStatsEvent,    'listenByCompetition', $this->createCompetition(1), 'match_stats@competition.1'],
-            [$matchStatsEvent,    'listenByTeam',        $this->createTeam(1),        'match_stats@team.1'],
+            [$matchStatsEvent,    'listenByMatch',          $this->createMatch(1),                  'match_stats@match.1'],
+            [$matchStatsEvent,    'listenByCompetition',    $this->createCompetition(1),            'match_stats@competition.1'],
+            [$matchStatsEvent,    'listenByTeam',           $this->createTeam(1),                   'match_stats@team.1'],
 
             // MatchLineup
-            [$matchLineupEvent,   'listenByMatch',       $this->createMatch(1),       'match_lineup@match.1'],
-            [$matchLineupEvent,   'listenByCompetition', $this->createCompetition(1), 'match_lineup@competition.1'],
-            [$matchLineupEvent,   'listenByTeam',        $this->createTeam(1),        'match_lineup@team.1'],
-            [$matchLineupEvent,   'listenByAthlete',     $this->createAthlete(1),     'match_lineup@athlete.1'],
+            [$matchLineupEvent,   'listenByMatch',          $this->createMatch(1),                  'match_lineup@match.1'],
+            [$matchLineupEvent,   'listenByCompetition',    $this->createCompetition(1),            'match_lineup@competition.1'],
+            [$matchLineupEvent,   'listenByTeam',           $this->createTeam(1),                   'match_lineup@team.1'],
+            [$matchLineupEvent,   'listenByAthlete',        $this->createAthlete(1),                'match_lineup@athlete.1'],
         ];
     }
 
