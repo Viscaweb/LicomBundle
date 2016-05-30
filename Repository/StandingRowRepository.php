2ba36bcd5aa0a792ec63c611b63b7ea6b599c7bd
Ricard Osorio <celtric@gmail.com>
2016-05-30 10:39:32 +0200

    add repositories related to standings

---
+++ Repository/StandingRowRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Repository;
+
+use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
+
+class StandingRowRepository extends AbstractEntityRepository
+{
+}
