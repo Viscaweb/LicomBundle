739ae0b7d8f74cf6ee90d27f02737b09661737bb
Jose Cornide <jose.cornide@dirital.com>
2016-07-18 11:41:16 +0200

    LIBE-661 - Implementation of the new transformer: Enetpulse - BettingOffer: repositories needed to create harmonizers

---
+++ Repository/BettingOfferRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Repository;
+
+use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
+
+/**
+ * Class BettingOfferRepository.
+ */
+class BettingOfferRepository extends AbstractEntityRepository
+{
+}
