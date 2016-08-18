6d0663ff8bd1b91867d21ad33407f8b75790df3b
Kristian <kristian@viscaweb.com>
2016-08-18 09:37:09 +0200

    LIFE - 459 - New method to find offers by Outcome Id's

--- Repository/BettingOfferRepository.php
+++ Repository/BettingOfferRepository.php
@@ -9,4 +9,27 @@ use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
  */
 class BettingOfferRepository extends AbstractEntityRepository
 {
+
+    /**
+     * Retuns all the offers from the given outcome ids
+     *
+     * @param array $outcomeIds
+     *
+     * @return array
+     */
+    public function findByOutcomeIds($outcomeIds = array()){
+
+        if(empty($outcomeIds)){
+            return [];
+        }
+
+        $queryBuilder = $this->createQueryBuilder('o');
+
+        $queryBuilder
+            ->where('o.bettingOutcome IN (:outcomeIds)')
+            ->setParameter('outcomeIds', $outcomeIds);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
 }
