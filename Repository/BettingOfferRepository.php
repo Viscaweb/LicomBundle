5787c9501144afa83bee447bd8d067705e54cf5d
Kristian <kristian@viscaweb.com>
2016-08-24 11:55:42 +0200

    LIFE - 459 - New methods to get the correct data for the providers

--- Repository/BettingOfferRepository.php
+++ Repository/BettingOfferRepository.php
@@ -2,6 +2,7 @@
 
 namespace Visca\Bundle\LicomBundle\Repository;
 
+use Doctrine\ORM\Query\Expr\Join;
 use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
 
 /**
@@ -52,5 +53,77 @@ class BettingOfferRepository extends AbstractEntityRepository
             ->setParameter('providerIds', $providerIds);
 
         return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * Returns all the offers from the given outcome ids and provider ids
+     *
+     * @param array $outcomeIds
+     * @param array $bookmakerKeys
+     * @param int   $bookmakersLimit
+     *
+     * @return array
+     *
+     */
+    public function findByOutcomeIdsAndBookmakerKeys($outcomeIds = array(), $bookmakerKeys = array(), $bookmakersLimit = 3)
+    {
+        if (empty($outcomeIds) || empty($bookmakerKeys)) {
+            return [];
+        }
+
+        $queryBuilder = $this
+            ->createQueryBuilder('o')
+            ->join('o.bettingOfferProvider', 'p')
+            ->join(
+                'ViscaLicomBundle:Bookmaker',
+                'b',
+                Join::WITH,
+                'b.provider = p.id'
+            )
+            ->where('o.bettingOutcome IN (:outcomeIds)')
+            ->andWhere('b.id IN (:bookmakerKeys)')
+            ->setParameter('outcomeIds', $outcomeIds)
+            ->setParameter('bookmakerKeys', $bookmakerKeys)
+            ->setMaxResults($bookmakersLimit);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * Returns all the offers from the given outcome ids and provider ids
+     *
+     * @param array $outcomeIds
+     * @param array $bookmakerKeys
+     * @param int   $bookmakersLimit
+     *
+     * @return array
+     *
+     */
+    public function findProviderIdsByOutcomeIdsAndBookmakerKeys($outcomeIds = array(), $bookmakerKeys = array(), $bookmakersLimit = 3)
+    {
+        if (empty($outcomeIds) || empty($bookmakerKeys)) {
+            return [];
+        }
+
+        $queryBuilder = $this
+            ->createQueryBuilder('o')
+            ->select('p.id')
+            ->join('o.bettingOfferProvider', 'p')
+            ->join(
+                'ViscaLicomBundle:Bookmaker',
+                'b',
+                Join::WITH,
+                'b.provider = p.id'
+            )
+            ->where('o.bettingOutcome IN (:outcomeIds)')
+            ->andWhere('b.id IN (:bookmakerKeys)')
+            ->setParameter('outcomeIds', $outcomeIds)
+            ->setParameter('bookmakerKeys', $bookmakerKeys)
+            ->setMaxResults($bookmakersLimit)
+            ->groupBy('p.id');
+
+        $providerIds = $queryBuilder->getQuery()->getScalarResult();
+
+        return array_column($providerIds, 'id');
     }
 }
