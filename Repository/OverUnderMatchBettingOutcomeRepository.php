a6e83cc5b28993cdef54e6cdb64b35480608168d
Kristian <kristian@viscaweb.com>
2016-08-25 17:58:58 +0200

    LIFE - 616 - Added new repository

---
+++ Repository/OverUnderMatchBettingOutcomeRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Repository;
+
+/**
+ * Class OverUnderMatchBettingOutcome.
+ */
+class OverUnderMatchBettingOutcomeRepository extends BettingOutcomeRepository
+{
+}
