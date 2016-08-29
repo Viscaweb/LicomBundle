c6c2a4c849424911046b4e96939e74d2c0491b4a
Félix Carpena <felix@viscaweb.com>
2016-08-29 18:08:14 +0200

    Because Life need to take care of the timestamp when the event was published, I'm enriching the Event structure and also keep the backward compatibility

--- Model/Consumers/Event.php
+++ Model/Consumers/Event.php
@@ -10,16 +10,21 @@ final class Event
     /** @var string */
     private $objectId;
 
+    /** @var \DateTimeImmutable */
+    private $timestamp;
+
     /**
      * Event constructor.
      *
      * @param string $eventName
      * @param string $objectId
+     * @param \DateTimeImmutable|null $timestamp
      */
-    public function __construct($eventName, $objectId)
+    public function __construct($eventName, $objectId, \DateTimeImmutable $timestamp = null)
     {
         $this->eventName = $eventName;
         $this->objectId = $objectId;
+        $this->timestamp = $timestamp ?: new \DateTimeImmutable('now', new \DateTimeZone('UTC'));
     }
 
     /**
@@ -36,5 +41,25 @@ final class Event
     public function getObjectId()
     {
         return $this->objectId;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @return string
+     */
+    public function toJson()
+    {
+        return json_encode([
+            'eventName' => $this->eventName,
+            'objectId' => $this->objectId,
+            'timestamp' => $this->timestamp->format('Y-m-d H:i:s'),
+        ]);
     }
 }
