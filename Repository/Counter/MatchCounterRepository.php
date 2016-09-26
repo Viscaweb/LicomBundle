c9c0982037aabd2f1e973a5d5d40deba06d03a65
Félix Carpena <felix@viscaweb.com>
2016-09-26 16:21:37 +0200

    Refactor query count live matches to just have in consideration matches started in the last 24 hours

--- Repository/Counter/MatchCounterRepository.php
+++ Repository/Counter/MatchCounterRepository.php
@@ -159,6 +159,7 @@ class MatchCounterRepository
             ->join('competitionCategory.sport', 'sport')
             ->where('sport.id = :sportId')
             ->andWhere('MatchStatusDescription.category = :statusCategory')
+            ->andWhere("DATE_ADD(m.startDate,1, 'day') > CURRENT_DATE()")
             ->setParameter('statusCategory', MatchStatusDescription::IN_PROGRESS_KEY)
             ->setParameter('sportId', $sport->getId());
 
