36d85ada947f3dab1a2b2dc0bd085fcfefb6800d
Jose Cornide <jose.cornide@dirital.com>
2016-11-09 17:15:32 +0100

    LIBE-896 Enetpulse - Implement new SportData transformer Standing  - Adding the standing promotion repository, needed to create the harmonized object finder from sport radar promotion to licom standing promotion

---
+++ Repository/StandingPromotionRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Repository;
+
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
+use Visca\Bundle\LicomBundle\Entity\Code\StandingColumnCode;
+use Visca\Bundle\LicomBundle\Entity\Code\StandingTypeCode;
+use Visca\Bundle\LicomBundle\Entity\Standing;
+
+/**
+ * Class StandingRepository.
+ */
+class StandingPromotionRepository extends AbstractEntityRepository
+{
+}
