c82b506116cdee54458e75878dec3227c0d4e4aa
ViscaWeb <system@viscaweb.com>
2016-11-09 16:15:45 +0000

    Applied fixes from StyleCI

--- Repository/StandingPromotionRepository.php
+++ Repository/StandingPromotionRepository.php
@@ -2,11 +2,7 @@
 
 namespace Visca\Bundle\LicomBundle\Repository;
 
-use Doctrine\ORM\Mapping\ClassMetadata;
 use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
-use Visca\Bundle\LicomBundle\Entity\Code\StandingColumnCode;
-use Visca\Bundle\LicomBundle\Entity\Code\StandingTypeCode;
-use Visca\Bundle\LicomBundle\Entity\Standing;
 
 /**
  * Class StandingRepository.
