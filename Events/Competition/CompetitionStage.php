ef0e56baf7fd3875135485d9bdecba44457e4712
Jonas HAOUZI <jonas@viscaweb.com>
2016-12-03 00:12:28 +0100

    Fixing tests

--- Events/Competition/CompetitionStage.php
+++ Events/Competition/CompetitionStage.php
@@ -5,10 +5,11 @@ namespace Visca\Bundle\LicomBundle\Events\Competition;
 use Visca\Bundle\LicomBundle\Events\AbstractEvent;
 use Visca\Bundle\LicomBundle\Events\Traits\ListenByCompetitionLegTrait;
 use Visca\Bundle\LicomBundle\Events\Traits\ListenByCompetitionStageTrait;
+use Visca\Bundle\LicomBundle\Events\Traits\ListenByCompetitionTrait;
 
 final class CompetitionStage extends AbstractEvent
 {
-    use ListenByCompetitionStageTrait, ListenByCompetitionLegTrait;
+    use ListenByCompetitionStageTrait, ListenByCompetitionLegTrait, ListenByCompetitionTrait;
 
     /**
      * @return string
