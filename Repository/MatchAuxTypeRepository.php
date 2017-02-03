7211617b19924e3c5355a0e186b524fe7c22c539
Raúl Ferràs <raul@viscaweb.com>
2017-02-03 13:07:01 +0100

    New repository for MatchAuxTypeRepository.

---
+++ Repository/MatchAuxTypeRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Repository;
+
+use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
+
+/**
+ * Class MatchAuxTypeRepository
+ */
+class MatchAuxTypeRepository extends AbstractEntityRepository
+{
+}
