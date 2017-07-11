d61b8deeb6ddfec59ae14e65386735a7e407d445
Jose Cornide <jose.cornide@dirital.com>
2017-07-11 16:02:35 +0200

    LIBE-1123 Add MatchParticipantAuxType repository

---
+++ Repository/MatchParticipantAuxTypeRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Repository;
+
+use Visca\Bundle\DoctrineBundle\Repository\Abstracts\AbstractEntityRepository;
+
+/**
+ * Class MatchParticipantAuxTypeRepository.
+ */
+class MatchParticipantAuxTypeRepository extends AbstractEntityRepository
+{
+}
