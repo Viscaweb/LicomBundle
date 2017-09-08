0bb9c40a82b46b2c75241111cbbcfd41c4d783d6
Raúl Ferràs <raul@viscaweb.com>
2017-09-08 18:17:42 +0200

    New doctrine enum type.

---
+++ Entity/Enum/DevicePlatformType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Visca\Bundle\LicomBundle\Entity\Enum;
+
+use Fresh\DoctrineEnumBundle\DBAL\Types\AbstractEnumType;
+
+/**
+ * Class DevicePlatformType
+ */
+class DevicePlatformType extends AbstractEnumType
+{
+    const APNS = 'apns';
+    const GCM = 'gcm';
+
+    protected $name = 'DevicePlatformType';
+
+    protected static $choices = [
+        self::APNS => self::APNS,
+        self::GCM => self::GCM
+    ];
+}
